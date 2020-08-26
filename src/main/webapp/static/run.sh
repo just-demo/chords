47841579991aa2b3f0d43271294703a044165ecc
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2020-08-26 19:12:35 +0300

    Update songs

--- src/main/webapp/static/run.sh
+++ src/main/webapp/static/run.sh
@@ -1,3 +1,3 @@
-python -m webbrowser http://localhost:8888
-#python -m http.server 8888
-python server.py
+python3 -m webbrowser http://localhost:8888
+#python3 -m http.server 8888
+python3 server.py
